95599b3af6aa06f5398db22b02fc914d08c46d78
Maduki-tech <maduki.business@gmail.com>
2023-04-03 19:19:33 +0200

    added sphere and ray

--- main.cpp
+++ main.cpp
@@ -4,20 +4,46 @@
 #include <fstream>
 #include <iostream>
 
-bool hit_sphere(const point3 &center, double radius, const ray &r) {
-    vec3 oc = r.origin() - center;
-    auto a = dot(r.direction(), r.direction());
-    auto b = 2.0 * dot(oc, r.direction());
-    auto c = dot(oc, oc) - radius * radius;
-    auto discriminant = b * b - 4 * a * c;
-    return (discriminant > 0);
+/**
+ * @brief If the ray hits this object it will paint a color whcih is given
+ *
+ * @param center
+ * @param radius
+ * @param r
+ * @return
+ */
+double hit_sphere(const point3 &center, double radius, const ray &r) {
+    vec3 originToCenter = r.origin() - center;
+    auto directionDotProduct = dot(r.direction(), r.direction());
+    auto originCenterDotDirection = 2.0 * dot(originToCenter, r.direction());
+    auto originCenterLenghtSquared =
+        dot(originToCenter, originToCenter) - radius * radius;
+    auto discriminant = originCenterDotDirection * originCenterDotDirection -
+                        4 * directionDotProduct * originCenterLenghtSquared;
+    if (discriminant < 0) {
+        return -1.0;
+    } else {
+        return (-originCenterDotDirection - sqrt(discriminant)) /
+               (2.0 * directionDotProduct);
+    }
 }
 
+/**
+ * @brief Colors the Background image and if there is something that is hitting
+ * an object it will paint it $ t = 0.5 * (direction + 1.0) $ $ blenvalue =
+ * (1-t) * color1 + t * color2 $
+ *
+ * @param r
+ * @return
+ */
 color ray_color(const ray &r) {
-    if (hit_sphere(point3(0, 0, -1), 0.5, r))
-        return color(1, 0, 0);
+    auto t = hit_sphere(point3(0, 0, -1), 0.5, r);
+    if (t > 0.0) {
+        vec3 N = unit_vector(r.at(t) - vec3(0, 0, -1));
+        return 0.5 * color(N.x() + 1, N.y() + 1, N.z() + 1);
+    }
     vec3 unit_direction = unit_vector(r.direction());
-    double t = 0.5 * (unit_direction.y() + 1.0);
+    t = 0.5 * (unit_direction.y() + 1.0);
     return (1.0 - t) * color(1.0, 1.0, 1.0) + t * color(0.5, 0.7, 1.0);
 }
 
@@ -35,7 +61,7 @@ int main(int argc, char *argv[]) {
     const double viewport_height = 2.0;
     const double viewport_width = aspect_ratio * viewport_height;
     // zoom length
-    const double focal_length = 0.3;
+    const double focal_length = 1.0;
 
     // where the camera is xyz
     const point3 origin(0, 0, 0);
