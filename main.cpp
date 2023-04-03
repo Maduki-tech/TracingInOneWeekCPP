5c199b3c35967d16aac8e3b22188108e3f90b714
Maduki-tech <maduki.business@gmail.com>
2023-04-03 17:20:53 +0200

    added ray and sphere

--- main.cpp
+++ main.cpp
@@ -1,10 +1,50 @@
 #include "includes/color.h"
+#include "includes/ray.h"
 #include "includes/vec3.h"
 #include <fstream>
 #include <iostream>
+
+bool hit_sphere(const point3 &center, double radius, const ray &r) {
+    vec3 oc = r.origin() - center;
+    auto a = dot(r.direction(), r.direction());
+    auto b = 2.0 * dot(oc, r.direction());
+    auto c = dot(oc, oc) - radius * radius;
+    auto discriminant = b * b - 4 * a * c;
+    return (discriminant > 0);
+}
+
+color ray_color(const ray &r) {
+    if (hit_sphere(point3(0, 0, -1), 0.5, r))
+        return color(1, 0, 0);
+    vec3 unit_direction = unit_vector(r.direction());
+    double t = 0.5 * (unit_direction.y() + 1.0);
+    return (1.0 - t) * color(1.0, 1.0, 1.0) + t * color(0.5, 0.7, 1.0);
+}
+
 int main(int argc, char *argv[]) {
-    const int image_width = 500;
-    const int image_height = 500;
+
+    // Image
+    // Bessere auflosung von dem bild
+    // Berechtnet die perfecte height des bildes aus
+    const auto aspect_ratio = 16.0 / 9.0;
+    const int image_width = 800;
+    const int image_height = static_cast<int>(image_width / aspect_ratio);
+
+    // Camera
+    // How much the camera can see
+    const double viewport_height = 2.0;
+    const double viewport_width = aspect_ratio * viewport_height;
+    // zoom length
+    const double focal_length = 0.3;
+
+    // where the camera is xyz
+    const point3 origin(0, 0, 0);
+    const vec3 horizontal(viewport_width, 0, 0);
+    const vec3 vertical(0, viewport_height, 0);
+    const point3 lower_left_corner =
+        origin - horizontal / 2 - vertical / 2 - vec3(0, 0, focal_length);
+    std::cout << lower_left_corner << std::endl;
+
     std::ofstream file("image.ppm", std::ios::out | std::ios::binary);
 
     file << "P6" << std::endl;
@@ -13,10 +53,12 @@ int main(int argc, char *argv[]) {
 
     for (int j = image_height - 1; j >= 0; --j) {
         for (int i = 0; i < image_width; ++i) {
-            color pixel_color(double(i) / (image_width - 1),
-                              double(j) / (image_height - 1), 0.25);
+            auto u = double(i) / (image_width - 1);
+            auto v = double(j) / (image_height - 1);
+            ray ray(origin,
+                    lower_left_corner + u * horizontal + v * vertical - origin);
+            color pixel_color = ray_color(ray);
             write_color(file, pixel_color);
-
         }
     }
 
